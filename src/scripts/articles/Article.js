fea370b2e5d9fc82eafb579f0b1e060d005b0e1f
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-19 09:30:39 -0500

    trying to get it to print

--- src/scripts/articles/Article.js
+++ src/scripts/articles/Article.js
@@ -3,9 +3,8 @@ export function Article(articleObject){
   return `
   <section class="article-card">
       <p>Title: ${articleObject.title}</p>
-      <p>Date: ${noteObject.date}</p>
-      <p>Suspect: ${criminalObject.name}</p>
-      <button id="deleteNote--${noteObject.id}">Delete</button>
+      <p>Synopsis: ${articleObject.synopsis}</p>
+      <p>URL: ${articleObject.url}</p>
   </section>
   `
 }
